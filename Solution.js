fb31ce6d5fc8b134303bf806f7333a924a3463f3
Lachezar Kutsarov <lkutsarov@gmail.com>
2026-02-20 00:45:46 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,23 @@
+
+/**
+ * @param {string} binary
+ * @return {number}
+ */
+var countBinarySubstrings = function (binary) {
+    let currentCount = 1;
+    let previousCount = 0;
+    let totalBinarySubstrings = 0;
+
+    for (let i = 1; i < binary.length; ++i) {
+        if (binary.charAt(i) === binary.charAt(i - 1)) {
+            ++currentCount;
+        } else {
+            totalBinarySubstrings += Math.min(previousCount, currentCount);
+            previousCount = currentCount;
+            currentCount = 1;
+        }
+    }
+    totalBinarySubstrings += Math.min(previousCount, currentCount);
+
+    return totalBinarySubstrings;
+};
